7f15592426c2dddc3226ba71b5f901e4aca39a66
Albin VIGIER <avigieri@linagora.com>
2024-04-07 18:26:34 +0200

    config du manifest.json pour l'appli PWA

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  pwa: {
+    name: 'Outils pour LNS',
+    short_name: 'Outils JDR LNS',
+    description: 'Fournit des outils pour le jeu de rôle LNS : dictionnaire des phases, recherche, etc...',
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#ffffff',
+    theme_color: '#41b383',
+  },
 })
